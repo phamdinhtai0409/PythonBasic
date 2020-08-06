0c6d1518fa14f307d0991c3e4fd893445409419b
Pham Dinh Tai <phamdinhtai0409@gmail.com>
2020-08-06 17:29:15 +0700

    test

--- bai10.py
+++ bai10.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+x = int(input("Nhap x: "))
+y = int(input("Nhap y: "))
+
+arr = np.zeros((x, y), dtype= int)
+
+for i in range (0, x):
+    for j in range (0, y):
+        arr[i,j] = i*j
+print(arr)
+
